f3f27829828377c92f12541fa79df23ba71f097b
Craig Presti <craig@vastpark.com>
2011-07-20 14:53:38 -0400

    Added extensions to retrieve MaterialGroup, Material, Texture, Video and Web elements from within IMaterialHostElement instances

--- src/Imml/Extensions.cs
+++ src/Imml/Extensions.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
+using Imml.Scene;
+using Imml.Scene.Controls;
+using Imml.ComponentModel;
 
 namespace Imml
 {
@@ -34,6 +37,58 @@ namespace Imml
                     }
                 }
             }
-        }   
+        }
+
+        /// <summary>
+        /// Gets the material group with the given Id.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="element">The element.</param>
+        /// <param name="id">The id.</param>
+        /// <returns></returns>
+        public static MaterialGroup GetMaterialGroup<T>(this T element, int id) where T : IMaterialHostElement
+        {
+            return element.Elements.OfType<MaterialGroup>().Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the material element from the material group.
+        /// </summary>
+        /// <param name="materialGroup">The material group.</param>
+        /// <returns></returns>
+        public static Material GetMaterial(this MaterialGroup materialGroup)
+        {
+            return materialGroup.Elements.OfType<Material>().FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the texture element from the material group.
+        /// </summary>
+        /// <param name="materialGroup">The material group.</param>
+        /// <returns></returns>
+        public static Texture GetTexture(this MaterialGroup materialGroup)
+        {
+            return materialGroup.Elements.OfType<Texture>().FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the web element from the material group.
+        /// </summary>
+        /// <param name="materialGroup">The material group.</param>
+        /// <returns></returns>
+        public static Web GetWeb(this MaterialGroup materialGroup)
+        {
+            return materialGroup.Elements.OfType<Web>().FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the video element from the material group.
+        /// </summary>
+        /// <param name="materialGroup">The material group.</param>
+        /// <returns></returns>
+        public static Video GetVideo(this MaterialGroup materialGroup)
+        {
+            return materialGroup.Elements.OfType<Video>().FirstOrDefault();
+        }
     }
 }
